903e040b8cf63f929c1d7aa79bc181f481555d58
Sahil Patel <jonyydeep555@gmail.com>
2023-08-25 14:51:35 +0530

    sahil

---
+++ resources/views/admin/manage_category.blade.php
@@ -0,0 +1,14 @@
+@extends('admin/layout')
+
+@section('container')
+<h1>Category</h1>
+<a href="manage_category">
+<button type="button" class="btn btn-success">Add Category</button>
+</a>
+<div class="row m-t-30">
+    <div class="col-md-12">
+       
+    </div>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ Route::post('admin/auth', [AdminController::class,'auth'])->name('admin.auth');
 Route::group(['middleware'=>'admin_auth'],function(){
     Route::get('admin/dashboard', [AdminController::class,'dashboard']);
     Route::get('admin/category', [CategoryController::class,'index']);
+    Route::get('admin/manage_category', [CategoryController::class,'manage_category']);
 });
 
  
